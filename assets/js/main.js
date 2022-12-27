feab9809112eed4c8a471ba26361a6b3967eab34
s0alken <ropedent@gmail.com>
2022-12-27 20:41:21 -0300

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 /*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHiddenPass = (inputPass, inputIcon) => {
+    const input = document.getElementById(inputPass);
+    const iconEye = document.getElementById(inputIcon);
+
+    iconEye.addEventListener('click', () => {
+
+        const values = {
+            password: { type: 'text', class: 'ri-eye-off-line' },
+            text: { type: 'password', class: 'ri-eye-line' }
+        }
+
+        input.type = values[input.type].type;
+        iconEye.className = `${values[input.type].class} login__eye`;
+
+    })
+}
+
+showHiddenPass('input-pass', 'input-icon');
